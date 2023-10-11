cdef3a6f14bc3594dadb6a9cf0b9ad0fb09bda56
mr-cheng111 <1959711225@qq.com>
2023-10-11 22:23:49 +0800

    finish the debug setting.
    It is hard to use! The Setting is in platformio.ini.

--- Wireless_Lidar/src/main.cpp
+++ Wireless_Lidar/src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
-#include <lidar.hpp>
 
-WIFI_Data_t Wifi_input;
+// #include <lidar.hpp>
+
+// WIFI_Data_t Wifi_input;
 
 
-
-void setup() {
-  //Serial.begin(115200);
-  Wifi_input.Wifi_SSID = "Mi10";
-  Wifi_input.Pass_Word ="12345678";
-  Wifi_input.Host_Ip   = "192.168.235.59";
-  Wifi_input.Port      = 8888;
-  lidar_t *L = new lidar_t(Wifi_input,0);
+uint32_t Counter = 0;
+void setup() 
+{
+  Serial.begin(115200);
+  
+  // Wifi_input.Wifi_SSID = "Mi10";
+  // Wifi_input.Pass_Word ="12345678";
+  // Wifi_input.Host_Ip   = "192.168.235.59";
+  // Wifi_input.Port      = 8888;
+  // lidar_t *L = new lidar_t(Wifi_input,0);
 }
 
 void loop() 
 {
-  // Serial.printf("AAA");
+  
+  Serial.printf("AAA");
+  Counter ++;
   delay(1000);
 }
 
