c4e3556019a7d84cc369bef9adad2ae9239b97b9
mr-cheng111 <1959711225@qq.com>
2023-10-11 23:32:17 +0800

    Testing Task's Priority. Currently I set the Priority of Micro_ROS_Task into "0"， It look good.

--- Wireless_Lidar/src/main.cpp
+++ Wireless_Lidar/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
-// #include <lidar.hpp>
+#include <lidar.hpp>
 
-// WIFI_Data_t Wifi_input;
+WIFI_Data_t Wifi_input;
 
 
 uint32_t Counter = 0;
@@ -10,18 +10,19 @@ void setup()
 {
   Serial.begin(115200);
   
-  // Wifi_input.Wifi_SSID = "Mi10";
-  // Wifi_input.Pass_Word ="12345678";
-  // Wifi_input.Host_Ip   = "192.168.235.59";
-  // Wifi_input.Port      = 8888;
-  // lidar_t *L = new lidar_t(Wifi_input,0);
+  Wifi_input.Wifi_SSID = "Xiaomi_WIFI";
+  Wifi_input.Pass_Word = "12345678910";
+  Wifi_input.Host_Ip   = "192.168.31.197";
+  Wifi_input.Port      = 8888;
+  lidar_t *L = new lidar_t(Wifi_input,1);
 }
 
 void loop() 
 {
   
-  Serial.printf("AAA");
+  Serial.printf("Counter : %d\r\n",Counter);
   Counter ++;
   delay(1000);
+
 }
 
