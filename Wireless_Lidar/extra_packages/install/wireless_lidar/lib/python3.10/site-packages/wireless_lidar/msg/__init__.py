24a8dd4c05f3f3dff337b86d2482957be77b26f1
mr-cheng111 <1959711225@qq.com>
2023-11-04 18:43:10 +0800

    Remove files from remote repository

--- Wireless_Lidar/extra_packages/install/wireless_lidar/lib/python3.10/site-packages/wireless_lidar/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from wireless_lidar.msg._lidar_data import LidarData  # noqa: F401
-from wireless_lidar.msg._point_data import PointData  # noqa: F401
--- Wireless_Lidar/extra_packages/install/wireless_lidar/share/wireless_lidar/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(wireless_lidar_IDL_FILES "msg/PointData.idl;msg/LidarData.idl")
-set(wireless_lidar_INTERFACE_FILES "src/PointData.msg;src/LidarData.msg")
